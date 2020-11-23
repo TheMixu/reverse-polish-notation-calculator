60b4a64da88ac82bb3fce3dca741c3811c16f96b
Mika <49287947+TheMixu@users.noreply.github.com>
2020-11-23 20:03:28 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const stack = [];
 let currentNum = '';
 
-function enter(){
+function enterNumber(){
+  if(currentNum === '' || currentNum === '.') return;
   stack.push(parseFloat(currentNum));
   document.getElementById('output').textContent = '';
   let string = '';
@@ -17,7 +18,16 @@ function pushNumber(number){
   document.getElementById('output').textContent = `${currentNum}`;
 }
 
-function turn(){
+function removeLastDigit(){
+  let str = '';
+  for (let i = 0; i < currentNum.length - 1 ; i += 1) {
+    str += currentNum[i];
+  }
+  currentNum = str;
+  document.getElementById('output').textContent = `${currentNum}`;
+}
+
+function turnNumber(){
   if(Number(currentNum) > 0){
     currentNum = (`-${currentNum}`);
   } else if (Number(currentNum) < 0) {
@@ -26,7 +36,7 @@ function turn(){
   document.getElementById('output').textContent = `${currentNum}`;
 }
 
-function calculate (operand) {
+function calculate(operand) {
   if(stack.length < 2 ){
     alert('not enough operands');
     return;
@@ -49,8 +59,8 @@ function calculate (operand) {
   document.getElementById('stack').textContent = string;
 }
 
-function clear(full){
-  if(full){
+function clear(fullClear){
+  if(fullClear){
     document.getElementById('stack').textContent = '';
     while(stack.length !== 0) {
       stack.pop();
@@ -62,68 +72,24 @@ function clear(full){
 
 document.getElementById('clear').addEventListener('click', () => clear(false));
 document.getElementById('clear-all').addEventListener('click', () => clear(true));
-document.getElementById("enter").addEventListener('click', enter);
-window.addEventListener('keydown', (event)=> {
-  if (event.defaultPrevented) {
-    return; // Do nothing if event already handled
+document.getElementById("enter").addEventListener('click', enterNumber);
+window.addEventListener('keyup', (event)=> {
+  if (event.isComposing) {
+    return;
   }
- 
-  switch(event.key) {
-    case "1":
-      pushNumber('1');
-      break;
-    case "2":
-      pushNumber('2');
-      break;
-    case "3":
-      pushNumber('3');
-       break;
-    case "4":
-      pushNumber('4');
-      break;
-    case "5":
-      pushNumber('5');
-      break;
-    case "6":
-      pushNumber('6');
-      break;
-    case "7":
-      pushNumber('7');
-      break;
-    case "8":
-      pushNumber('8');
-      break;
-    case "9":
-      pushNumber('9');
-      break;
-    case "0":
-      pushNumber('0');
-     break;
-    case "Enter":
-      enter();
-      break;
-    case "-":
-      calculate('-');
-      break;
-    case "+":
-      calculate('+');
-      break;
-    case "/":
-      calculate('/');
-      break;
-    case "*":
-      calculate('*');
-      break;
-    case ".":
-      pushNumber('.');
-      break;
-    case "Backspace":
-      clear(false);
-      break;
-    default: 
-      return;
+  const keyMap = {
+    '+': () => calculate('+'),
+    '-': () => calculate('-'),
+    '/': () => calculate('/'),
+    '*': () => calculate('*'),
+    'Enter': () => enterNumber(),
+    'Backspace': () => removeLastDigit(),
+    '.': () => pushNumber('.'),
+  };
+  if(event.key >= 0 && event.key <= 9){
+    pushNumber(event.key);
+  } else if (event.key in keyMap){
+    keyMap[event.key]();
   }
-  // Consume the event so it doesn't get handled twice
-  event.preventDefault();
 }, true);
-document.getElementById("turn").addEventListener('click', turn);
+document.getElementById("turn").addEventListener('click', turnNumber);
